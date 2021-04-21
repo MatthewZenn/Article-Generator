90a5fd6b220a0a6505faf6c33e1cb205d7e17309
Matthew Zenn <spyguy57@gmail.com>
2021-04-21 14:45:57 -0400

    Added project files

---
+++ logic.js
@@ -0,0 +1,35 @@
+const realButton = document.getElementById("picture");
+const fakeButton = document.getElementById("open");
+const image = document.getElementById("image");
+
+
+fakeButton.addEventListener("click", function() {
+    realButton.click()
+});
+
+realButton.addEventListener("change", function() {
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener("load", function() {
+            image.setAttribute('src', this.result);
+        });
+        reader.readAsDataURL(file);
+    }
+});
+
+function textinput() {
+    var title = (document.getElementById("title").value);
+    var caption = (document.getElementById("caption").value);
+    document.getElementById("output1").innerHTML = title;
+    document.getElementById("output2").innerHTML = caption;
+}
+
+function printresult() {
+    html2canvas(document.querySelector("#result")).then(canvas => {
+        document.body.appendChild(canvas).style.visibility = "hidden";
+        Canvas2Image.saveAsPNG(canvas, 600, 600);
+    });
+}
