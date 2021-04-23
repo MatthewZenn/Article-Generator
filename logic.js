2a5853e6d9d9a4aa916ae9abc711a2f4fbbc6959
Matthew Zenn <spyguy57@gmail.com>
2021-04-23 12:46:20 -0400

    updated beta

--- logic.js
+++ logic.js
@@ -2,6 +2,12 @@ const realButton = document.getElementById("picture");
 const fakeButton = document.getElementById("open");
 const image = document.getElementById("image");
 
+const realButton2 = document.getElementById("background-image");
+const fakeButton2 = document.getElementById("custom");
+const backgroundImage = document.getElementById("background");
+let outlets = ["news.png", "news2.png", "news3.png"]
+
+Index = 1;
 
 fakeButton.addEventListener("click", function() {
     realButton.click()
@@ -20,6 +26,23 @@ realButton.addEventListener("change", function() {
     }
 });
 
+fakeButton2.addEventListener("click", function() {
+    realButton2.click()
+});
+
+realButton2.addEventListener("change", function() {
+    const file2 = this.files[0];
+
+    if (file2) {
+        const reader2 = new FileReader();
+
+        reader2.addEventListener("load", function() {
+            backgroundImage.setAttribute('src', this.result);
+        });
+        reader2.readAsDataURL(file2);
+    }
+});
+
 function textinput() {
     var title = (document.getElementById("title").value);
     document.getElementById("output1").innerHTML = title;
@@ -30,4 +53,14 @@ function printresult() {
         document.body.appendChild(canvas).style.visibility = "hidden";
         Canvas2Image.saveAsPNG(canvas, 600, 600);
     });
+}
+
+function changeSource()  {
+    if (Index < outlets.length) {
+        backgroundImage.setAttribute('src', outlets[Index]);
+        Index++;
+     }
+     else {
+        Index = 0;   
+     }
 }
